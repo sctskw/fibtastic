0f42cf7fa76d4af0595f35e4a67424cd73add72f
Scott Skow <sskow@layer3tv.com>
2018-07-23 01:32:48 -0600

    small adjustments to examples

--- examples/recursion.js
+++ examples/recursion.js
@@ -1,7 +1,5 @@
-function fib (fn1, fn2, max) {
+(function fib (fn1, fn2, max) {
   if (fn2 >= max) return false
   console.log(fn2)
   return fib(fn2, fn1 + fn2, max)
-}
-
-fib(0, 1, 4000000)
+})(0, 1, 4000000)
--- examples/squares.js
+++ examples/squares.js
@@ -1,12 +1,10 @@
 // NOTE: only useful for asking:
 //
 // what is the value of the fibonacci sequence at X
-
-function fibonacci (n) {
+let result = (function fibonacci (n) {
   let q = Math.pow((1 + Math.sqrt(5)) / 2, n)
   let r = Math.pow(-2 / (1 + Math.sqrt(5)), n)
-
   return Math.round((q - r) / Math.sqrt(5))
-}
+})(33)
 
-console.log(fibonacci(50))
+console.log(result)
